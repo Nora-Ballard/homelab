c80f09f44906ffa63b1f73656db9a8d17d4e4952
Nora Ballard <Nora-Ballard@users.noreply.github.com>
2020-11-05 11:23:22 -0600

    Remove clipboard dependency

--- scripts/update-udmp-cert.sh
+++ scripts/update-udmp-cert.sh
@@ -1,7 +1,10 @@
-ssh truenas cat /etc/certificates/unifi-core.key| pbcopy
-pbpaste | ssh unifi-udmp "cat > /mnt/data/unifi-os/unifi-core/config/unifi-core.key"
+ssh truenas "cat /etc/certificates/unifi-core.key" | ssh unifi-udmp "cat > /mnt/data/unifi-os/unifi-core/config/unifi-core.key"
+ssh truenas "md5 /etc/certificates/unifi-core.key"
+ssh unifi-udmp "md5sum /mnt/data/unifi-os/unifi-core/config/unifi-core.key"
  
-ssh truenas cat /etc/certificates/unifi-core.crt | pbcopy
-pbpaste | ssh unifi-udmp "cat > /mnt/data/unifi-os/unifi-core/config/unifi-core.crt"
+ 
+ssh truenas cat /etc/certificates/unifi-core.crt | ssh unifi-udmp "cat > /mnt/data/unifi-os/unifi-core/config/unifi-core.crt"
+
+
 
 ssh unifi-udmp /usr/sbin/unifi-os restart
