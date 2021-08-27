48e1c800e77b385f9c06a1ad9d9ea7120e8a4d22
Nora Ballard <4589305+Nora-Ballard@users.noreply.github.com>
2021-08-27 17:09:12 -0500

    Apply terraform fmt

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
       version = "~>3.1"
     }
     template = {
-      source = "hashicorp/template"
+      source  = "hashicorp/template"
       version = "~>2.2"
     }
   }
@@ -37,24 +37,24 @@ resource "maas_vm_host" "lxd" {
 }
 
 data "template_cloudinit_config" "kubernetes" {
-  gzip = false
+  gzip          = false
   base64_encode = false
 
   part {
     content_type = "text/cloud-config"
-    content = yamlencode(local.init_base)
-    merge_type = "list(append)+dict(recurse_array)+str()"
+    content      = yamlencode(local.init_base)
+    merge_type   = "list(append)+dict(recurse_array)+str()"
   }
   part {
     content_type = "text/cloud-config"
-    content = templatefile("${path.module}/cloud-config/kubernetes-core.yaml", {})
-    merge_type = "list(append)+dict(recurse_array)+str()"
+    content      = templatefile("${path.module}/cloud-config/kubernetes-core.yaml", {})
+    merge_type   = "list(append)+dict(recurse_array)+str()"
   }
-  
+
   part {
     content_type = "text/cloud-config"
-    content = templatefile("${path.module}/cloud-config/kubernetes-docker.yaml", {})
-    merge_type = "list(append)+dict(recurse_array)+str()"
+    content      = templatefile("${path.module}/cloud-config/kubernetes-docker.yaml", {})
+    merge_type   = "list(append)+dict(recurse_array)+str()"
   }
 }
 
@@ -63,7 +63,7 @@ resource "maas_instance" "kubernetes" {
 
   allocate_params {
     min_cpu_count = 2
-    min_memory = 8192 # MB
+    min_memory    = 8192 # MB
   }
 
   deploy_params {
